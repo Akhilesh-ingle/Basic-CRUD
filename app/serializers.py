4f4129ece134b31ee5120c431370732aa9982fb2
akhilesh <ingleakhilesh676@gmail.com>
2024-12-13 20:34:20 +0530

    added custom serializer methods

--- app/serializers.py
+++ app/serializers.py
@@ -5,3 +5,25 @@ class StudentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Student
         fields = '__all__'
+
+    # def validate(self, obj):
+    #     if obj.get('age') < 18:
+    #         raise serializers.ValidationError("Failed: Age should be greater than 18")
+        
+    #     if 'z' in obj.get('name'):
+    #         raise serializers.ValidationError("Failed: There should be no 'z' in name")
+        
+    #     return obj
+
+    def validate_age(self, age):
+        if age < 18:
+            raise serializers.ValidationError("Failed: Age should be greater than 18")
+        
+        return age
+    
+    def validate_name(self, name):
+        if 'z' in name:
+            raise serializers.ValidationError("Failed: There should be no 'z' in name")
+        
+        return name
+
--- app/views.py
+++ app/views.py
@@ -2,6 +2,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from app.models import Student
 from app.serializers import StudentSerializer
+from rest_framework import status
 
 # Create your views here.
 
@@ -14,14 +15,20 @@ class StudentsView(APIView):
     def get(self, request):
         students = Student.objects.all()
         serializer = StudentSerializer(students, many=True)
-        return Response({'msg': 'List of students', 'data': serializer.data})
+        return Response({'msg': 'List of students', 'data': serializer.data}, status=status.HTTP_200_OK)
     
     def post(self, request):
         data = request.data
+        
+        # if data.get('age') < 18:
+        #     return Response({'msg': 'Failed: Age should be greater than 18'})
+        
         serializer = StudentSerializer(data=data)
         if serializer.is_valid():
             serializer.save()
-            return Response({'msg': 'Record created successfully', 'data': serializer.data})
+            return Response({'msg': 'Record created successfully', 'data': serializer.data}, status=status.HTTP_201_CREATED)
+        else:
+            return Response({'msg': 'Validation Error', 'error': serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
 
 
 class StudentDetailView(APIView):
@@ -29,31 +36,31 @@ class StudentDetailView(APIView):
         try:
             student = Student.objects.get(id=id)
         except Student.DoesNotExist:
-            return Response({'msg': 'Invalid id'})
+            return Response({'msg': 'Invalid id'}, status=status.HTTP_400_BAD_REQUEST)
         
         serializer = StudentSerializer(student)
-        return Response({'msg': 'Student record', 'data': serializer.data})
+        return Response({'msg': 'Student record', 'data': serializer.data}, status=status.HTTP_200_OK)
     
     def put(self, request, id):
         try:
             student = Student.objects.get(id=id)
         except Student.DoesNotExist:
-            return Response({'msg': 'Invalid id'})
+            return Response({'msg': 'Invalid id'}, status=status.HTTP_400_BAD_REQUEST)
         
         data = request.data
         serializer = StudentSerializer(student, data=data, partial=True)
         if serializer.is_valid():
             serializer.save()
-            return Response({'msg': 'Record updated successsfully', 'data': serializer.data})
-        return Response({'msg': 'Validation Error', 'error': serializer.errors})
+            return Response({'msg': 'Record updated successsfully', 'data': serializer.data}, status=status.HTTP_200_OK)
+        return Response({'msg': 'Validation Error', 'error': serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
     
     def delete(self, request, id):
         try:
             student = Student.objects.get(id=id)
         except Student.DoesNotExist:
-            return Response({'msg': 'Invalid id'})
+            return Response({'msg': 'Invalid id'}, status=status.HTTP_400_BAD_REQUEST)
         
         student.delete()
-        return Response({'msg': 'Record deleted successfully'})
+        return Response({'msg': 'Record deleted successfully'}, status=status.HTTP_200_OK)
 
 
